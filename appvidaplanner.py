a4116f08e204d66863bab0b977eb5382f81d24a8
Lettiere69 <52424138+Lettiere69@users.noreply.github.com>
2025-05-16 16:56:05 -0300

    Update appvidaplanner.py

--- appvidaplanner.py
+++ appvidaplanner.py
@@ -40,12 +40,9 @@ if st.session_state.acesso_liberado:
     import requests
 
     def enviar_agenda_telegram(agenda, chat_id, bot_token):
-        mensagem = "Sua agenda do dia: "
-
-
+        mensagem = "📅 Sua agenda do dia:\n\n"
         for item in agenda:
-            mensagem += f"• {item} "
-
+            mensagem += f"• {item}\n"
 
 
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
